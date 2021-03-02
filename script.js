42abdaf0111533672d663d1b661a604e3907a9f0
its-pablo <78528552+its-pablo@users.noreply.github.com>
2021-03-02 08:55:28 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
 console.log("This website is being developed.");
 
-class OneDialog extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Biscourse</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li id="homepagenav"><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-    </ul>
-  </div>
-</nav>
-`;
-  }
-}
-
-customElements.define('one-dialog', OneDialog);
-
 function overlayOn() {
   document.getElementById("overlay").style.display = "block";
 }
