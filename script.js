04cf505b94f49c99fb78df893653b51c36aaef5d
its-pablo <78528552+its-pablo@users.noreply.github.com>
2021-03-02 08:50:43 -0800

    Changing header to reusable template

--- script.js
+++ script.js
@@ -31,3 +31,23 @@ function overlayOn() {
 function overlayOff() {
   document.getElementById("overlay").style.display = "none";
 }
+
+template.innerHTML = `<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Biscourse</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li id="homepagenav"><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+`
+
+document.body.appendChild(template.content);
